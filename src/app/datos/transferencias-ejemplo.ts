2bf63c827105c6ca7524ca88b4fa157104c5a003
Carlos250293 <cveragaray@gmail.com>
2023-06-14 14:09:56 +0200

    implementado transferencias

---
+++ src/app/datos/transferencias-ejemplo.ts
@@ -0,0 +1,53 @@
+export const transferenciasRecibidas = [
+    {
+        id: 1,
+        idOrdenante: 1,
+        idBeneficiario: 2,
+        importe: 50,
+        concepto: "Regalo cumpleaños",
+        fecha: new Date(),
+    },
+    {
+        id: 2,
+        idGestor:1,
+        idBeneficiario: 2,
+        importe: 1050,
+        concepto: "Nomina",
+        fecha: new Date(),
+    },
+    {
+        id: 4,
+        idGestor:1,
+        idBeneficiario: 2,
+        importe: 650,
+        concepto: "Alquiler",
+        fecha: new Date(),
+    },
+]
+
+export const transferenciasEnviadas = [
+    {
+        id: 1,
+        idOrdenante: 1,
+        idBeneficiario: 4,
+        importe: 300,
+        concepto: "Regalo boda",
+        fecha: new Date(),
+    },
+    {
+        id: 2,
+        idGestor:1,
+        idBeneficiario: 6,
+        importe: 25,
+        concepto: "Cena despedida",
+        fecha: new Date(),
+    },
+    {
+        id: 4,
+        idGestor:1,
+        idBeneficiario: 5,
+        importe: 2205,
+        concepto: "Traje boda",
+        fecha: new Date(),
+    },
+]
